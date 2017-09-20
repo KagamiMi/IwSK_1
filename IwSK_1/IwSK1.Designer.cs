f36f3d2a550aaaa0be192422a0c90b03d3d4f2f9
Roksana Paluch <rokspal895@student.polsl.pl>
2017-09-20 21:11:29 +0200

    Zmiana wyglądu i itemy w dropdownach

--- IwSK_1/IwSK1.Designer.cs
+++ IwSK_1/IwSK1.Designer.cs
@@ -34,18 +34,29 @@
             this.textBox1receivedDataTextBox = new System.Windows.Forms.TextBox();
             this.transmitButton = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.terminatorComboBox = new System.Windows.Forms.ComboBox();
+            this.flowControlComboBox = new System.Windows.Forms.ComboBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label11 = new System.Windows.Forms.Label();
+            this.speedTextBox = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.stopBitsComboBox = new System.Windows.Forms.ComboBox();
+            this.controlComboBox = new System.Windows.Forms.ComboBox();
+            this.dataFieldComboBox = new System.Windows.Forms.ComboBox();
             this.label9 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.portComboBox = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // transmitedDataTextBox
@@ -93,41 +104,136 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.terminatorComboBox);
+            this.panel1.Controls.Add(this.flowControlComboBox);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.label12);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.portComboBox);
-            this.panel1.Location = new System.Drawing.Point(23, 32);
+            this.panel1.Location = new System.Drawing.Point(12, 32);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(548, 223);
+            this.panel1.Size = new System.Drawing.Size(646, 223);
             this.panel1.TabIndex = 5;
+            // 
+            // terminatorComboBox
+            // 
+            this.terminatorComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.terminatorComboBox.FormattingEnabled = true;
+            this.terminatorComboBox.Location = new System.Drawing.Point(136, 86);
+            this.terminatorComboBox.Name = "terminatorComboBox";
+            this.terminatorComboBox.Size = new System.Drawing.Size(222, 24);
+            this.terminatorComboBox.TabIndex = 7;
+            // 
+            // flowControlComboBox
+            // 
+            this.flowControlComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.flowControlComboBox.FormattingEnabled = true;
+            this.flowControlComboBox.Location = new System.Drawing.Point(136, 56);
+            this.flowControlComboBox.Name = "flowControlComboBox";
+            this.flowControlComboBox.Size = new System.Drawing.Size(222, 24);
+            this.flowControlComboBox.TabIndex = 6;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(53, 89);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(77, 17);
+            this.label13.TabIndex = 5;
+            this.label13.Text = "Terminator";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(2, 59);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(128, 17);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "Kontrola przepływu";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(322, 9);
+            this.label5.Location = new System.Drawing.Point(374, 9);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(160, 17);
             this.label5.TabIndex = 3;
             this.label5.Text = "Parametry transmisyjne:";
-            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.label9);
-            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label11);
+            this.panel2.Controls.Add(this.speedTextBox);
+            this.panel2.Controls.Add(this.panel3);
             this.panel2.Controls.Add(this.label6);
-            this.panel2.Controls.Add(this.label10);
             this.panel2.Controls.Add(this.label7);
-            this.panel2.Location = new System.Drawing.Point(322, 29);
+            this.panel2.Location = new System.Drawing.Point(374, 29);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(214, 180);
+            this.panel2.Size = new System.Drawing.Size(249, 180);
             this.panel2.TabIndex = 2;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(167, 16);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(34, 17);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "bit/s";
+            // 
+            // speedTextBox
+            // 
+            this.speedTextBox.Location = new System.Drawing.Point(87, 13);
+            this.speedTextBox.Name = "speedTextBox";
+            this.speedTextBox.Size = new System.Drawing.Size(73, 22);
+            this.speedTextBox.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.stopBitsComboBox);
+            this.panel3.Controls.Add(this.controlComboBox);
+            this.panel3.Controls.Add(this.dataFieldComboBox);
+            this.panel3.Controls.Add(this.label9);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Controls.Add(this.label10);
+            this.panel3.Location = new System.Drawing.Point(11, 63);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(224, 106);
+            this.panel3.TabIndex = 2;
+            // 
+            // stopBitsComboBox
+            // 
+            this.stopBitsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.stopBitsComboBox.FormattingEnabled = true;
+            this.stopBitsComboBox.Location = new System.Drawing.Point(105, 66);
+            this.stopBitsComboBox.Name = "stopBitsComboBox";
+            this.stopBitsComboBox.Size = new System.Drawing.Size(116, 24);
+            this.stopBitsComboBox.TabIndex = 8;
+            // 
+            // controlComboBox
+            // 
+            this.controlComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.controlComboBox.FormattingEnabled = true;
+            this.controlComboBox.Location = new System.Drawing.Point(105, 38);
+            this.controlComboBox.Name = "controlComboBox";
+            this.controlComboBox.Size = new System.Drawing.Size(116, 24);
+            this.controlComboBox.TabIndex = 7;
+            // 
+            // dataFieldComboBox
+            // 
+            this.dataFieldComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dataFieldComboBox.FormattingEnabled = true;
+            this.dataFieldComboBox.Location = new System.Drawing.Point(105, 8);
+            this.dataFieldComboBox.Name = "dataFieldComboBox";
+            this.dataFieldComboBox.Size = new System.Drawing.Size(116, 24);
+            this.dataFieldComboBox.TabIndex = 6;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(45, 140);
+            this.label9.Location = new System.Drawing.Point(38, 37);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(61, 17);
             this.label9.TabIndex = 4;
@@ -136,11 +242,20 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(20, 111);
+            this.label8.Location = new System.Drawing.Point(13, 11);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(86, 17);
             this.label8.TabIndex = 2;
             this.label8.Text = "Pole danych";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(26, 66);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 17);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "Bity stopu";
             // 
             // label6
             // 
@@ -151,28 +266,19 @@
             this.label6.TabIndex = 0;
             this.label6.Text = "Szybkość";
             // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(33, 78);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(73, 17);
-            this.label10.TabIndex = 5;
-            this.label10.Text = "Bity stop\'u";
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(12, 44);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(94, 17);
+            this.label7.Size = new System.Drawing.Size(98, 17);
             this.label7.TabIndex = 1;
-            this.label7.Text = "Format znaku";
+            this.label7.Text = "Format znaku:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 16);
+            this.label4.Location = new System.Drawing.Point(96, 29);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(34, 17);
             this.label4.TabIndex = 1;
@@ -182,26 +288,25 @@
             // 
             this.portComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.portComboBox.FormattingEnabled = true;
-            this.portComboBox.Location = new System.Drawing.Point(54, 13);
+            this.portComboBox.Location = new System.Drawing.Point(136, 26);
             this.portComboBox.Name = "portComboBox";
-            this.portComboBox.Size = new System.Drawing.Size(118, 24);
+            this.portComboBox.Size = new System.Drawing.Size(222, 24);
             this.portComboBox.TabIndex = 0;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(27, 6);
+            this.label3.Location = new System.Drawing.Point(18, 9);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(91, 17);
             this.label3.TabIndex = 6;
             this.label3.Text = "Konfiguracja:";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // IwSK1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(598, 533);
+            this.ClientSize = new System.Drawing.Size(670, 533);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.transmitButton);
@@ -215,6 +320,8 @@
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -238,6 +345,16 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox speedTextBox;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ComboBox stopBitsComboBox;
+        private System.Windows.Forms.ComboBox controlComboBox;
+        private System.Windows.Forms.ComboBox dataFieldComboBox;
+        private System.Windows.Forms.ComboBox terminatorComboBox;
+        private System.Windows.Forms.ComboBox flowControlComboBox;
+        private System.Windows.Forms.Label label13;
     }
 }
 
--- IwSK_1/IwSK1.cs
+++ IwSK_1/IwSK1.cs
@@ -12,22 +12,29 @@ namespace IwSK_1
 {
     public partial class IwSK1 : Form
     {
+        private List<string> dataField = new List<string>(new string[] {"7-bitowe","8-bitowe"});
+        private List<string> control = new List<string>(new string[] {"E (parzystość)","O (nieparzystość)","N (brak)"});
+        private List<string> stopBits = new List<string>(new string[] {"1 bit","2 bity"});
+        private List<string> flowControl = new List<string>(new string[] {"Brak kontroli przepływu","Sprzętowa - handshake DTR/DSR",
+        "Sprzętowa - handshake RTS/CTS", "Programowa - XON/XOFF"});
+        private List<string> terminator = new List<string>(new string[] {"Brak terminatora","Terminator standardowy CR","Terminator standardowy LF",
+        "Terminator standardowy CR-LF", "Terminator własny 1-znakowy", "Terminator własny 2-znakowy"});
         public IwSK1()
         {
             InitializeComponent();
             var list = SerialPort.GetPortNames();
             portComboBox.DataSource = list;
             portComboBox.SelectedIndex = -1;
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
+            dataFieldComboBox.DataSource = dataField;
+            dataFieldComboBox.SelectedIndex = -1;
+            controlComboBox.DataSource = control;
+            controlComboBox.SelectedIndex = -1;
+            stopBitsComboBox.DataSource = stopBits;
+            stopBitsComboBox.SelectedIndex = -1;
+            flowControlComboBox.DataSource = flowControl;
+            flowControlComboBox.SelectedIndex = -1;
+            terminatorComboBox.DataSource = terminator;
+            terminatorComboBox.SelectedIndex = -1;
         }
     }
 }
